38e4ea07c0bc47b7bdf3a8ebc8d49dbd4ae89448
fariba <fmassah@cj.com>
2018-12-10 22:55:29 -0800

    Start writing tests

--- src/test/scala/CryptoTest.scala
+++ src/test/scala/CryptoTest.scala
@@ -1,12 +1,16 @@
 
 import org.scalatest.{FlatSpec, Matchers}
+import com.seefaribacode.generateKeyPair
 
 class CryptoTest extends FlatSpec with Matchers {
 
+  val keyPair = generateKeyPair
   behavior of "encryptMessageToBytes"
 
   it should "encrypt a message to bytes" in {
-    ???
+    //given
+    val msg = "hello"
+
   }
 
 
